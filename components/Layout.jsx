7dc2f93a9cb972c293aee99396b7e22e86b5ba85
Brian Emil Hartz <brianhartz@gmail.com>
2015-04-18 17:29:38 -0600

    redo cleanup

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,34 +6,22 @@ var RouteHandler = Router.RouteHandler;
 var Layout = React.createClass({
 
   getDefaultProps: function() {
-    return {title: 'React Static Site'};
+
   },
 
   render: function() {
-    var script = (process.env.NODE_ENV!=='production') ? <script src="http://localhost:3000/scripts/bundle.js"></script> : '';
-    // var style = (process.env.NODE_ENV==='production') ? <link rel="stylesheet" href="/assets/pure.css" /> : '';
-    // var style2 = (process.env.NODE_ENV==='production') ? <link rel="stylesheet" href="/assets/style.css" /> : '';
 
-    var title = 'Test - React Static Site';
     return (
-      <html>
-        <head>
-          <title>{title}</title>
-        </head>
-        <body>
-          <div id="layout">
-            <ul>
-              <li><Link to="frolick" params={{frolickId: "123"}}>a123</Link></li>
-              <li><Link to="frolick" params={{frolickId: "1234"}}>b1234</Link></li>
-              <li><Link to="frolick" params={{frolickId: "12345"}}>12345</Link></li>
-            </ul>
-            <main role="main">
-              <RouteHandler {...this.props}/>
-            </main>
-          </div>
-          {script}
-        </body>
-      </html>
+      <div>
+        <ul>
+          <li><Link to="frolick" params={{frolickId: "123"}}>a123</Link></li>
+          <li><Link to="frolick" params={{frolickId: "1234"}}>b1234</Link></li>
+          <li><Link to="frolick" params={{frolickId: "12345"}}>12345</Link></li>
+        </ul>
+        <main role="main">
+          <RouteHandler {...this.props}/>
+        </main>
+      </div>
     );
   }
 });
